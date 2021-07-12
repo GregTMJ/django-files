e8afc1efa944f6b96b4d49447f83c4b3f5a7b8c9
GregTMJ <greg_1461994@hotmail.com>
2021-07-12 20:55:34 +0300

    'FinalProjectBackend'

---
+++ Final_project/PostBoard/Board/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Post, Category, Author, Comments
+# Register your models here.
+
+
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Comments)
